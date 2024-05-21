ec90087c91401663e74d4d25ecb8139d8ab84c29
KnightOfMelons <ilaprosmotrov@gmail.com>
2024-05-21 23:35:48 +0400

    Defining Dynamic Parameters for Impulse Response

--- app.py
+++ app.py
@@ -18,6 +18,38 @@ def plot_impulse_transient_response():
         x_values = [float(request.form[f"xValues_{i}"]) for i in range(17)]
         y_values = [float(request.form[f"yValues_{i}"]) for i in range(17)]
 
+        def calculate_total_area(x_values, y_values):
+            total_area = 0
+
+            for i in range(len(x_values) - 1):
+                width = x_values[i + 1] - x_values[i]
+                height1 = y_values[i]
+                height2 = y_values[i + 1]
+                area = 0.5 * (height1 + height2) * width
+                total_area += area
+
+            return total_area
+
+        def find_first_positive_index(y_values, x_values):
+            for index, value in enumerate(y_values):
+                if value > 0:
+                    return x_values[index]
+            return None  # Если в y_values нет значений больше нуля
+
+        # Получение данных из формы 2
+        tau = float(request.form["tau"])
+        x_values_imp = float(request.form["x_values_imp"])
+        at = float(request.form["at"])
+        a_theta = float(request.form["a_theta"])
+        M = float(request.form["M"])
+
+        Fl = (tau * at)
+        o = max(y_values)
+        Ta = round(Fl / max(y_values), 1)
+        Fo = calculate_total_area(x_values, y_values)
+        k = round(Fo / Fl, 1)
+        t = find_first_positive_index(y_values, x_values)
+
         # Создаем новую фигуру и оси
         fig, ax = plt.subplots()
 
@@ -40,7 +72,8 @@ def plot_impulse_transient_response():
         image = '<img src="data:image/png;base64,{}">'.format(image_base64)
 
         # Выводим HTML-код на сайт
-        return render_template('PID/impulse_transient_response.html', image=image)
+        return render_template('PID/impulse_transient_response.html', image=image,
+                               numbers=[tau, x_values_imp, at, a_theta, M, Ta, k, t, o, Fl, Fo])
 
     return render_template("PID/impulse_transient_response.html")
 
